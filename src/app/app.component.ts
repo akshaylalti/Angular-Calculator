b80e3ac4837b6da1a70758904fe6faff77281273
Akshay Kumar <akshaylalti@outlook.com>
2023-10-03 16:17:34 +0530

    Clear and equal button updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,122 +1,145 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+export class AppComponent implements OnInit {
+  pageTitle = 'calculator';
+  fun: any = 'noFunction';
+  funVal: string = '';
+  userInput: string = '';
+  res: number = 0;
+  calNum: string = 'noValue';
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+  isFunTemp: number = 0;
 
-export class AppComponent {
-  title = 'calculator';
-  fun:any='noFunction';
-  input:number=0;
-  calNum:string='noValue';
-  firstNumber:number = 0;
-  secondNumber:number = 0;
-  onClickValue(val:string,type:any){
-    if( type == 'number'){
-      this.CalNumber( val);
-    }
-    else if (type == 'function'){
-      this.callFunction(val);
-    }
+  constructor(private title: Title) {
+    this.title.setTitle(this.pageTitle);
   }
 
-  CalNumber( val:string){
-    if(this.calNum != 'noValue'){
-      this.calNum = this.calNum + val;
-    }else{
-      this.calNum = val;
-    }
-    this.input = parseFloat(this.calNum);
-  }
+  ngOnInit() {}
 
-  callFunction( val:string){
-    if( this.fun == 'C'){
+  onClickValue(val: string, type: any) {
+    debugger;
+    if (val != '=') {
+      this.userInput += val;
+    }
+
+    if (val != 'C') {
+      if (type == 'number') {
+        this.CalNumber(val);
+      } else if (type == 'function') {
+        this.fun = type;
+        if (val != '=') {
+          this.isFunTemp++;
+          this.funVal = val;
+        }
+        this.callFunction(val, type);
+      }
+    } else {
       this.ClearFun();
     }
-    else if( this.fun == 'noFunction'){
-      this.firstNumber = this.input;
-      this.calNum = 'noValue';
-      this.input = 0;
-      this.fun = val;
+  }
+
+  CalNumber(val: string) {
+    if (this.calNum != 'noValue') {
+      this.calNum = this.calNum + val;
+    } else {
+      this.calNum = val;
     }
-    else if(this.fun != 'noFunction'){
-      this.secondNumber = this.input;
+    this.res = parseFloat(this.calNum);
+  }
+
+  callFunction(val: string, type: string) {
+    if (type == 'function' && val != '=' && this.isFunTemp == 1) {
+      this.firstNumber = this.res;
+      this.calNum = 'noValue';
+      this.res = 0;
+      this.fun = val;
+    } else {
+      debugger;
+      this.secondNumber = this.res;
       this.calculator(val);
     }
   }
 
-  calculator(val : string)
-  {
-    if(this.fun == '+'){
+  calculator(val: string) {
+    if (this.funVal == '+') {
       const result = this.firstNumber + this.secondNumber;
-      this.input = result;
+      this.res = result;
       this.firstNumber = result;
       this.secondNumber = 0;
       this.calNum = 'noValue';
-      this.fun =  val;
-      if( val == '='){
+      this.fun = val;
+      if (val == '=') {
         this.EqualFun();
       }
     }
-    if(this.fun == '-'){
+    if (this.funVal == '-') {
       const result = this.firstNumber - this.secondNumber;
-      this.input = result;
+      this.res = result;
       this.firstNumber = result;
       this.secondNumber = 0;
       this.calNum = 'noValue';
-      this.fun =  val;
-      if( val == '='){
+      this.fun = val;
+      if (val == '=') {
         this.EqualFun();
       }
     }
-    if(this.fun == '*'){
-      const result= this.firstNumber * this.secondNumber;
-      this.input = result;
+    if (this.funVal == '*') {
+      const result = this.firstNumber * this.secondNumber;
+      this.res = result;
       this.firstNumber = result;
       this.secondNumber = 0;
       this.calNum = 'noValue';
-      this.fun =  val;
-      if( val == '='){
+      this.fun = val;
+      if (val == '=') {
         this.EqualFun();
       }
     }
-    if(this.fun == '/'){
-      const result= this.firstNumber / this.secondNumber;
-      this.input = result;
+    if (this.funVal == '/') {
+      const result = this.firstNumber / this.secondNumber;
+      this.res = result;
       this.firstNumber = result;
       this.secondNumber = 0;
       this.calNum = 'noValue';
-      this.fun =  val;
-      if( val == '='){
+      this.fun = val;
+      if (val == '=') {
         this.EqualFun();
       }
     }
-    if(this.fun == '%'){
+    if (this.funVal == '%') {
       const result = this.firstNumber % this.secondNumber;
-      this.input = result;
+      this.res = result;
       this.firstNumber = result;
       this.secondNumber = 0;
       this.calNum = 'noValue';
-      this.fun =  val;
-      if( val == '='){
+      this.fun = val;
+      if (val == '=') {
         this.EqualFun();
       }
     }
   }
 
-
-  EqualFun(){
+  EqualFun() {
     this.firstNumber = 0;
     this.secondNumber = 0;
     this.calNum = 'noValue';
     this.fun = 'nofunction';
+    this.isFunTemp = 0;
+    this.funVal = '';
   }
-  ClearFun(){
+  ClearFun() {
     this.firstNumber = 0;
     this.secondNumber = 0;
     this.calNum = 'noValue';
     this.fun = 'nofunction';
-    this.input = 0;
+    this.res = 0;
+    this.userInput = '';
+    this.funVal = '';
+    this.isFunTemp = 0;
   }
 }
